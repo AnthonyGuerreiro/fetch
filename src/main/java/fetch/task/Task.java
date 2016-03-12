4c418f16c8430c9865b2e69cfaaf01a6a36e9444
Anthony Guerreiro <anthonyepf@gmail.com>
2016-03-12 19:17:44 +0000

    Add Task and TaskLoader

---
+++ src/main/java/fetch/task/Task.java
@@ -0,0 +1,21 @@
+package fetch.task;
+
+public interface Task {
+
+    /**
+     * Executes task.
+     */
+    void execute();
+
+    /**
+     * Returns order. <br />
+     * Order is used to specify when this task will run compared to other tasks.
+     * <br />
+     * The order returned should be equal or greater than 0.<br />
+     * If two tasks have the same order, it is not certain which task will run
+     * first.
+     *
+     * @return order.
+     */
+    int getOrder();
+}
