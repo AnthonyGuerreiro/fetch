cb33726e56829c8b14dddca37c75a97347584384
Anthony Guerreiro <anthonyepf@gmail.com>
2016-03-22 11:22:13 +0000

    Add Fetch logger

--- src/main/java/fetch/core/Fetch.java
+++ src/main/java/fetch/core/Fetch.java
@@ -3,10 +3,14 @@ package fetch.core;
 import java.util.List;
 
 import fetch.exception.ConfigurationException;
+import fetch.log.LogManager;
+import fetch.log.Logger;
 import fetch.plugin.PluginLoader;
 import fetch.task.Task;
 
 public class Fetch {
+
+    private final static Logger logger = LogManager.getLogger(Fetch.class);
 
     public static void main(String[] args) throws ConfigurationException {
         new Fetch().start(args);
@@ -18,6 +22,8 @@ public class Fetch {
 
     private void start(String[] args) throws ConfigurationException {
         List<Task> tasks = getTasks();
+
+        logger.info("Executing tasks");
         for (Task task : tasks) {
             task.execute();
             task.onFinish();
