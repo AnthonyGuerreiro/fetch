af1a0212ea8f79011094ac3b9af9e35aca9f1eb7
Anthony Guerreiro <anthonyepf@gmail.com>
2016-03-22 11:10:08 +0000

    Remove generic TestEvents: test events in proper task tester class

--- src/test/java/fetch/test/task/reader/TestProfileReader.java
+++ src/test/java/fetch/test/task/reader/TestProfileReader.java
@@ -1,14 +1,29 @@
 package fetch.test.task.reader;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import fetch.exception.ConfigurationException;
 import fetch.task.reader.Reader;
+import fetch.task.searcher.ShowSearcher;
+import fetch.test.AbstractTest;
 
-public class TestProfileReader {
+public class TestProfileReader extends AbstractTest {
 
     @Test
     public void testLoadProfileReader() throws ConfigurationException {
         new Reader().getProfilesReader();
+    }
+
+    @Test
+    public void testSimpleProfilesReadEventHandler() throws ConfigurationException {
+        Reader reader = getTask(Reader.class).get();
+        reader.getProfiles().add(getProfile("profile"));
+
+        ShowSearcher showSearcher = getTask(ShowSearcher.class).get();
+        assertEquals(0, showSearcher.getProfiles().size());
+        reader.onFinish();
+        assertEquals(1, showSearcher.getProfiles().size());
     }
 }
